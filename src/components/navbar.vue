f47428d6535d49e224b24adeff8e0ff8341db38b
headre <45737374+headre@users.noreply.github.com>
2020-04-20 09:59:58 +0800

    fix logout and login

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -8,8 +8,8 @@
           </div>
           <div class="phone">
             <h5><span class="glyphicon glyphicon-phone" aria-hidden="true"></span>+86-***-****-****</h5>
-            <div v-if="isLogin=='false'"><button @click="changeStatus">login</button> </div>
             <div v-if="isLogin=='true'"><button @click="logout">logout</button> </div>
+            <div v-else><button @click="changeStatus">login</button> </div>
           </div>
           <div class="clearfix"></div>
         </div>
